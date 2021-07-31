fd14d1f268fe8c7d70ebfb64e06c1d193c5cb8db
Ali Haider <alihaidder_pilot@hotmail.com>
2021-07-31 23:03:03 +1000

    Added services unit test

---
+++ InsuranceWebApp.Tests/Services/InsuranceServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using AutoBogus;
+using InsuranceWebApp.Data;
+using InsuranceWebApp.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InsuranceWebApp.Tests.Services
+{
+    [TestClass]
+    public class InsuranceServiceTests
+    {
+        private InsuranceDetail request;
+        [TestInitialize]
+        public void Setup()
+        {
+            request = new AutoFaker<InsuranceDetail>().Generate();
+        }
+
+        [TestMethod]
+        public void When_CalculateInsurance_Then_Response()
+        {
+            var sut = new InsuranceService();
+            var result = sut.CalculateInsurance(request);
+            Assert.IsInstanceOfType(result, typeof(double));
+            Assert.IsNotNull(result);
+        }
+    }
+}
